42db22f93aa27cecd01822a53a058d5b81bad5aa
minhthu01 <85405233+minhthu01@users.noreply.github.com>
2021-06-09 22:59:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Thương Mơ phải không !',
-    introDesc: `Thương thích ăn gì nhất`,
-    btnIntro: 'đoán xem',
-    title: ' Một là ăn cut hai là ăn phân',
-    desc: ' Đồng ý là ăn cả hhahah',
-    btnYes: 'Tao ăn hết <33',
+    introTitle: 'Quang Dự à !',
+    introDesc: `Bắt nạt em ít thôi đừng để em nóng hơi mệt :))`,
+    btnIntro: 'Anh đồng ý',
+    title: ' ',
+    desc: ' Yêu em vô bờ bến',
+    btnYes: 'Không làm chó cái <33',
     btnNo: 'Không ăn làm chó cũng ăn hứt:3',
     question:'Ngoài bố láo ra còn gì tốt đẹp không',
     btnReply: 'Gửi cho em <3',
     reply: 'Có chứ t còn dâm dê với khẩu nghiệp cơ',
     mess: 'Thế thì vứt con đi lol ơiiii -.-',
-    messDesc: ' Tóm lại là khẩu nghiệp ít thôi cho t khẩu nghiệp với hahah.',
+    messDesc: ' Tóm lại là  Đanh đá ít thôi đừng để tôi đấm cho không trượt phát nào hahah.',
     btnAccept: 'Em hay quá :D <33',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
