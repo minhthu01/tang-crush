2094911bf1389a547e5d59cd51d87fbbc57b5774
minhthu01 <85405233+minhthu01@users.noreply.github.com>
2021-06-09 22:04:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'Quang Dự à!',
+    introDesc: `Bắt nạt em ít thôi đừng để em nóng hơi mệt`,
+    btnIntro: 'Em thông minh quá',
+    title: 'Anh có biết sự giống nhau giữa anh và Hà Nội là gì không làm sao mà biết được 😙',
+    desc: 'Đó là đều được em yêu 1000 năm',
+    btnYes: 'Yêu em vô bờ bến <33',
+    btnNo: 'Không làm chó cái:3',
+    question:'Tại sao anh không đẹp trai không giàu như người ta mà em lại chọn anh <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Vì anh là điều tuyệ vời nhất với em rồi đừng so sánh anh với người khác',
+    mess: 'Em không văn nhá-.- Anh thật là ngu ngoc zl😘',
+    messDesc: 'Đanh đá ít thôi đừng để tôi đấm.',
+    btnAccept: 'Em hay quá hahah <33',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
