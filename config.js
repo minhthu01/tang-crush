0c0c924ceb8d4b93f4c7f9a28aac3cad3c24b329
minhthu01 <85405233+minhthu01@users.noreply.github.com>
2021-06-09 22:22:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'Quang Dự à!',
     introDesc: `Bắt nạt em ít thôi đừng để em nóng hơi mệt`,
-    btnIntro: 'Em thông minh quá',
+    btnIntro: 'Anh đồng ý',
     title: 'Anh có biết sự giống nhau giữa anh và Hà Nội là gì không làm sao mà biết được 😙',
-    desc: 'Đó là đều được em yêu 1000 năm',
+    desc: 'Đó là đều được em yêu 1000 năm hhahah',
     btnYes: 'Yêu em vô bờ bến <33',
     btnNo: 'Không làm chó cái:3',
     question:'Tại sao anh không đẹp trai không giàu như người ta mà em lại chọn anh <3',
     btnReply: 'Gửi cho em <3',
-    reply: 'Vì anh là điều tuyệ vời nhất với em rồi đừng so sánh anh với người khác',
+    reply: 'Vì anh là điều tuyệt vời nhất với em rồi đừng so sánh anh với bất kì ai',
     mess: 'Em không văn nhá-.- Anh thật là ngu ngoc zl😘',
-    messDesc: 'Đanh đá ít thôi đừng để tôi đấm.',
-    btnAccept: 'Em hay quá hahah <33',
+    messDesc: 'Đanh đá ít thôi đừng để tôi đấm cho không trượt phát nào :)).',
+    btnAccept: 'Em hay quá :D <33',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
